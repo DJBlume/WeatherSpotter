ea72ad92839daf54d33721fda8d7b537e5d2c3f9
DJ Blume <dj.blume9@gmail.com>
2020-12-12 19:12:26 -0600

    update build

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,6 +4,7 @@ import Moment from 'moment';
 
 export default class Forecast extends Component {
 
+
     render() {
         //today data
         const todForecast = this.props.forecast[0];
@@ -16,24 +17,22 @@ export default class Forecast extends Component {
         return (
             <div className="forecastContainer">
                 <div className="forecastday">
-                    <div>
-                        <h3>Today</h3>
-                        <div className="description">
-                            <img src={todForecast.icon} alt="weather icon"></img>
-                            {todForecast.desc}
-                        </div>
-                        <div className="forecastTemp">
-                            <p>High: {imperial && Math.round(todForecast.highF)}{!imperial && Math.round(todForecast.highC)}</p>
-                            <p>Low: {imperial && Math.round(todForecast.lowF)}{!imperial && Math.round(todForecast.lowC)}</p>
-                        </div>
-                        <div className="precip">
-                            <p>Chance of Rain: {todForecast.rain}%</p>
-                            <p>Chance of Snow: {todForecast.snow}%</p>
-                            <p>Expected Precipitation: {imperial && todForecast.totalPrecip_in + "in"}{!imperial && todForecast.totalPrecip_mm + "mm"}</p>
-                        </div>
-                        <div className="wind">
-                            Wind: {imperial && todForecast.windSpdM + "mph"}{!imperial && todForecast.windSpdK + "kph"}
-                        </div>
+                    <h3>Today</h3>
+                    <div className="description">
+                        <img src={todForecast.icon} alt="weather icon"></img>
+                        {todForecast.desc}
+                    </div>
+                    <div className="forecastTemp">
+                        <p>High: {imperial && Math.round(todForecast.highF)}{!imperial && Math.round(todForecast.highC)}&deg;{imperial && "F"}{!imperial && "C"}</p>
+                        <p>Low: {imperial && Math.round(todForecast.lowF)}{!imperial && Math.round(todForecast.lowC)}&deg;{imperial && "F"}{!imperial && "C"}</p>
+                    </div>
+                    <div className="precip hidden">
+                        <p>Chance of Rain: {todForecast.rain}%</p>
+                        <p>Chance of Snow: {todForecast.snow}%</p>
+                        <p>Expected Precipitation: {imperial && todForecast.totalPrecip_in + "in"}{!imperial && todForecast.totalPrecip_mm + "mm"}</p>
+                    </div>
+                    <div className="wind hidden">
+                        Wind: {imperial && todForecast.windSpdM + "mph"}{!imperial && todForecast.windSpdK + "kph"}
                     </div>
                 </div>
                 <div className="forecastday">
@@ -43,15 +42,15 @@ export default class Forecast extends Component {
                         {todForecast.desc}
                     </div>
                     <div className="forecastTemp">
-                        <p>High: {imperial && Math.round(tomForecast.highF)}{!imperial && Math.round(tomForecast.highC)}</p>
-                        <p>Low: {imperial && Math.round(tomForecast.lowF)}{!imperial && Math.round(tomForecast.lowC)}</p>
+                        <p>High: {imperial && Math.round(tomForecast.highF)}{!imperial && Math.round(tomForecast.highC)}&deg;{imperial && "F"}{!imperial && "C"}</p>
+                        <p>Low: {imperial && Math.round(tomForecast.lowF)}{!imperial && Math.round(tomForecast.lowC)}&deg;{imperial && "F"}{!imperial && "C"}</p>
                     </div>
-                    <div className="precip">
+                    <div className="precip hidden">
                         <p>Chance of Rain: {tomForecast.rain}%</p>
                         <p>Chance of Snow: {tomForecast.snow}%</p>
                         <p>Expected Precipitation: {imperial && tomForecast.totalPrecip_in + "in"}{!imperial && tomForecast.totalPrecip_mm + "mm"}</p>
                     </div>
-                    <div className="wind">
+                    <div className="wind hidden">
                         Wind: {imperial && tomForecast.windSpdM + "mph"}{!imperial && tomForecast.windSpdK + "kph"}
                     </div>
                 </div>
@@ -62,15 +61,15 @@ export default class Forecast extends Component {
                         {dayForecast.desc}
                     </div>
                     <div className="forecastTemp">
-                        <p>High: {imperial && Math.round(dayForecast.highF)}{!imperial && Math.round(dayForecast.highC)}</p>
-                        <p>Low: {imperial && Math.round(dayForecast.lowF)}{!imperial && Math.round(dayForecast.lowC)}</p>
+                        <p>High: {imperial && Math.round(dayForecast.highF)}{!imperial && Math.round(dayForecast.highC)}&deg;{imperial && "F"}{!imperial && "C"}</p>
+                        <p>Low: {imperial && Math.round(dayForecast.lowF)}{!imperial && Math.round(dayForecast.lowC)}&deg;{imperial && "F"}{!imperial && "C"}</p>
                     </div>
-                    <div className="precip">
+                    <div className="precip hidden">
                         <p>Chance of Rain: {dayForecast.rain}%</p>
                         <p>Chance of Snow: {dayForecast.snow}%</p>
                         <p>Expected Precipitation: {imperial && dayForecast.totalPrecip_in + "in"}{!imperial && dayForecast.totalPrecip_mm + "mm"}</p>
                     </div>
-                    <div className="wind">
+                    <div className="wind hidden">
                         Wind: {imperial && dayForecast.windSpdM + "mph"}{!imperial && dayForecast.windSpdK + "kph"}
                     </div>
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header(props) {
             <form className="location" onSubmit={(e) => { props.updateWeather(e) }}>
                 <label htmlFor="location">Search Location: </label>
                 <input className="location_input" name="location" placeholder="City/Zip" onChange={(e) => props.changeLocation(e.target.value)}></input>
-                <input className="submit" type="submit"></input>
+                <button className="submit" type="submit" value="search"></button>
             </form>
             <div className="toggleContainer">
                 <button className="toggleUnits" onClick={(e) => props.handleClick(e)}>Imperial/Metric</button>
